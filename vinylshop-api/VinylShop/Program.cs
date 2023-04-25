8c5156f34972b974cd81e9683e650d239a0406e8
HudemaDana <hudema.dana@gmail.com>
2023-04-25 08:47:20 +0300

    remove auto-migrate

--- vinylshop-api/VinylShop/Program.cs
+++ vinylshop-api/VinylShop/Program.cs
@@ -53,11 +53,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//    dbContext.Database.Migrate();
+//}
 
 app.UseCors("Allow all");
 
