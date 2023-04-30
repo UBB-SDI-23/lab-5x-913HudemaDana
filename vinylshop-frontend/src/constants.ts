cb885569dcda90b9d8941d63e77ebda7bb63e411
HudemaDana <hudema.dana@gmail.com>
2023-04-30 23:04:16 +0300

    updated constants for production

--- vinylshop-frontend/src/constants.ts
+++ vinylshop-frontend/src/constants.ts
@@ -1,3 +1,5 @@
+const PROD_BACKEND_API_URL = "https://vinylshop-mpp.netlify.app/api";
 const DEV_BACKEND_API_URL = "http://localhost:1234/api";
 
-export const BACKEND_API_URL =DEV_BACKEND_API_URL;
+export const BACKEND_API_URL =
+	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
